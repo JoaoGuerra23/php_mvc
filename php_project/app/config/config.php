48a7ad11814cdede369b62cf7a97027383b14f3a
JoaoGuerra23 <joao_guerra23@hotmail.com>
2022-07-25 08:38:00 +0100

    First commit

---
+++ php_project/app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+//DB Params
+
+define('DB_HOST', 'sql-master1');
+define('DB_USER', 'sandbox');
+define('DB_PASS', 'sandbox');
+define('DB_NAME', 'sandbox');
+
+//App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL root
+define('URLROOT', 'http://sandbox.exads.rocks');
+
+// Site Name
+define('SITENAME', 'SharePosts');
+// App Version
+define('APPVERSION', '1.0.0');
---
+++ php_project/app/libraries/Core.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * App Core Class
+ * Creates URL & Loads Core Controller
+ * URL FORMAT - /controller/method/params
+ */
+
+class Core
+{
+
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct()
+    {
+        //$this->getUrl();
+
+        $url = $this->getUrl();
+
+        //Look in controllers for controller for first value
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            //If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            //Unset 0 Index
+            unset($url[0]);
+
+        }
+
+        //Require de controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        //Instantiate controller class
+        $this->currentController = new $this->currentController;
+        //var_dump($url, $_GET, $_SERVER['REQUEST_URI']);
+        //exit();
+
+        //Check for second part of url
+        if (isset($url[1])) {
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+
+                unset($url[1]);
+            }
+        }
+
+        //Get params
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+
+
+    }
+
+    public function getUrl()
+    {
+
+        if (isset($_SERVER['REQUEST_URI'])) {
+
+            $url = ltrim($_SERVER['REQUEST_URI'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            return explode('/', $url);
+
+        }
+
+    }
+
+}
